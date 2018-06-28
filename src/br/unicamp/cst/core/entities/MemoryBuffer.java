b4c55bf0bef2e98912b5b8b8ce07fc49a9c7ef57
andre-paraense <andre.paraense@mobwise.com.br>
2018-06-28 19:13:19 -0300

    - api specification for Memory Buffer

--- src/br/unicamp/cst/core/entities/MemoryBuffer.java
+++ src/br/unicamp/cst/core/entities/MemoryBuffer.java
@@ -11,58 +11,50 @@
 
 package br.unicamp.cst.core.entities;
 
-//import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
-
-
-
 /**
- * MemoryBuffer is a generic holder for memory objects. It may be 
- * used to produce sensory buffers, action buffers or other very short 
- * term memory structures.
+ * MemoryBuffer is a generic holder for memory objects. It may be used to
+ * produce sensory buffers, action buffers or other very short term memory
+ * structures.
  * 
- * @author klaus.raizer
+ * @author K. Raizer
  */
 
-public class MemoryBuffer 
-{
-	//Using CopyOnWriteArrayList in this case is not ideal because we have many writes compared to the number of reads..
-	private ArrayList<MemoryObject>  memoryObjects; //A memory buffer is essentially a fifo list of MO
+public class MemoryBuffer {
+
+	/** A memory buffer is essentially a fifo list of MO */
+	private ArrayList<MemoryObject> memoryObjects;
+
 	private int maxcapacity;
-	// Safe lock for multithread access
-	public volatile Lock lock= new ReentrantLock();
-	
+
+	/** Safe lock for multithread access */
+	public volatile Lock lock = new ReentrantLock();
+
 	private RawMemory rawMemory;
-	
+
 	/**
-	 * Safe access through reentrant locks
+	 * Safe access through reentrant locks.
 	 * 
 	 * @param accesing
-	 * @return
+	 *            the accessing Codelet
+	 * @return true is is impeding access
 	 */
-	public boolean impendingAccess(Codelet accesing)//TODO: Probably useless at the moment
-	{
+	public boolean impendingAccess(Codelet accesing) {
 		Boolean myLock = false;
 		Boolean yourLock = false;
-		try
-		{
+		try {
 			myLock = lock.tryLock();
 			yourLock = accesing.lock.tryLock();
-		} finally
-		{
-			if (!(myLock && yourLock))
-			{
-				if (myLock)
-				{
+		} finally {
+			if (!(myLock && yourLock)) {
+				if (myLock) {
 					lock.unlock();
 				}
-				if (yourLock)
-				{
+				if (yourLock) {
 					accesing.lock.unlock();
 				}
 			}
@@ -71,162 +63,174 @@ public class MemoryBuffer
 	}
 
 	/**
-	 * Default constructor
-	 * @param maxcapacity maximum number of elements this buffer holds at a given time
-	 * @param rawMemory singleton instance of the system's raw memory
+	 * Creates a MemoryBuffer.
+	 * 
+	 * @param maxcapacity
+	 *            maximum number of elements this buffer holds at a given time.
+	 * @param rawMemory
+	 *            singleton instance of the system's raw memory.
 	 */
-	public MemoryBuffer(int maxcapacity, RawMemory rawMemory)
-	{
-		memoryObjects=new ArrayList<MemoryObject>();
-		this.maxcapacity=maxcapacity;
-		//	      this.lock = new ReentrantLock();
-		
+	public MemoryBuffer(int maxcapacity, RawMemory rawMemory) {
+		memoryObjects = new ArrayList<MemoryObject>();
+		this.maxcapacity = maxcapacity;
 		this.rawMemory = rawMemory;
 	}
 
-
 	/**
-	 * @param contents list of Memory Objects to be added
+	 * Adds a lits of memory objects.
+	 * 
+	 * @param contents
+	 *            list of Memory Objects to be added
 	 */
-	public synchronized void putList(List<MemoryObject> contents)
-	{
+	public synchronized void putList(List<MemoryObject> contents) {
 
-		for(MemoryObject thisContent:contents){
-			if(memoryObjects.size()==maxcapacity){
-				//  System.out.println("Popping..");
-				memoryObjects.remove(0); //Gets rid of older content
+		for (MemoryObject thisContent : contents) {
+			if (memoryObjects.size() == maxcapacity) {
 
+				memoryObjects.remove(0); // Gets rid of older content
 
 			}
 			memoryObjects.add(thisContent);
 		}
 
 	}
+
 	/**
+	 * Adds one memory object.
 	 * 
-	 * @param content one memory object to be added
+	 * @param content
+	 *            one memory object to be added.
 	 */
 
-	public synchronized void put(MemoryObject content)
-	{
+	public synchronized void put(MemoryObject content) {
 
-		if(memoryObjects.size()==maxcapacity)
-		{
-			if(rawMemory!=null)
-				rawMemory.destroyMemoryObject(memoryObjects.get(0));//Gets rid of older content and deletes it from raw memory
+		if (memoryObjects.size() == maxcapacity) {
+			if (rawMemory != null)
+				rawMemory.destroyMemoryObject(memoryObjects.get(0));// Gets rid
+																	// of older
+																	// content
+																	// and
+																	// deletes
+																	// it from
+																	// raw
+																	// memory
 			memoryObjects.remove(0);
 		}
 
 		memoryObjects.add(content);
 
-
 	}
 
 	/**
-	 * Returns the original list of memory objects in this buffer
-	 * @return the list of Memory objects inside Buffer
+	 * Returns the original list of memory objects in this buffer.
+	 * 
+	 * @return the list of Memory objects inside Buffer.
 	 */
-	public synchronized List<MemoryObject> get()
-	{
+	public synchronized List<MemoryObject> get() {
 		return memoryObjects;
 	}
+
 	/**
 	 * Returns a copy of the list of memory objects in this buffer.
 	 * 
-	 * @return the list of Memory objects inside Buffer
+	 * @return the list of Memory objects inside Buffer.
 	 */
-	public synchronized ArrayList<MemoryObject> getAll()
-	{
-		ArrayList<MemoryObject> all=new ArrayList<MemoryObject>();
+	public synchronized ArrayList<MemoryObject> getAll() {
+		ArrayList<MemoryObject> all = new ArrayList<MemoryObject>();
 		all.addAll(this.memoryObjects);
 		return memoryObjects;
 	}
+
 	/**
-	 * @return pops the first memory object that went into this list
+	 * Pops the first memory object that went into this list.
+	 * 
+	 * @return the first memory object that went into this list.
 	 */
-	public synchronized MemoryObject pop()
-	{
+	public synchronized MemoryObject pop() {
 		MemoryObject mo = memoryObjects.get(0);
 		memoryObjects.remove(0);
 		return mo;
 	}
 
 	/**
-	 * @return size of the list of Memory objects inside  Buffer
-	 */  
-	public synchronized int size()
-	{
+	 * Gets the size of the list of Memory objects inside Buffer.
+	 * 
+	 * @return size of the list of Memory objects inside Buffer.
+	 */
+	public synchronized int size() {
 		return memoryObjects.size();
 	}
+
 	/**
-	 * @return the most recent Memory Object of this type
+	 * Gets the most recent Memory Object of this type.
+	 * 
+	 * @return the most recent Memory Object of this type.
 	 */
 	public synchronized MemoryObject getMostRecent() {
 		MemoryObject mostRecent;
-		//			System.out.println("Getting most recent");
-		if (memoryObjects.size()>0){
-			mostRecent  = memoryObjects.get(memoryObjects.size()-1);
-			//				System.out.println("--Buffer content--"+mostRecent);
 
-		}
-		else
-		{
-			//				System.out.println("Buffer is empty");
-			mostRecent=null;
+		if (memoryObjects.size() > 0) {
+			mostRecent = memoryObjects.get(memoryObjects.size() - 1);
+
+		} else {
+
+			mostRecent = null;
 		}
 
 		return mostRecent;
 	}
-/**
- * 
- * @return the oldest MemoryObject in this list
- */
-	public synchronized MemoryObject getOldest(){
-//		System.out.println("Current content: "+memoryObjects);
+
+	/**
+	 * Gets the oldest MemoryObject in this list.
+	 * 
+	 * @return the oldest MemoryObject in this list.
+	 */
+	public synchronized MemoryObject getOldest() {
 		MemoryObject oldest;
-		if (memoryObjects.size()>0){
-			oldest=memoryObjects.get(0);
+		if (memoryObjects.size() > 0) {
+			oldest = memoryObjects.get(0);
+		} else {
+			oldest = null;
 		}
-		else
-		{
-			oldest=null;
-		}
-		return oldest;			
-	}
-
-
-	/**
-	 * Removes this memory object from this buffer and eliminates it from raw memory
-	 * @param mo memory object to be removed
-	 * @return result of of the removal process
-	 */
-	public synchronized boolean remove(MemoryObject mo)
-	{
-		if(rawMemory!=null)
-			rawMemory.destroyMemoryObject(mo);//removes this mo form RawMemory
-
-		return memoryObjects.remove(mo);//removes this mo from this buffer;
-
+		return oldest;
 	}
 
 	/**
-	 * Clears all memory objects from this buffer
+	 * Removes this memory object from this buffer and eliminates it from raw
+	 * memory.
+	 * 
+	 * @param mo
+	 *            memory object to be removed.
+	 * @return result of of the removal process.
 	 */
-	public synchronized void clear() 
-	{
-		//			memoryObjects.clear();		
+	public synchronized boolean remove(MemoryObject mo) {
+		if (rawMemory != null)
+			rawMemory.destroyMemoryObject(mo);// removes this mo form RawMemory
 
-		if(rawMemory!=null)
-		for(int i=0;i<memoryObjects.size();i++)
-		{			
-			rawMemory.destroyMemoryObject(memoryObjects.get(i)); 
-		}
+		return memoryObjects.remove(mo);// removes this mo from this buffer;
+
+	}
+
+	/**
+	 * Clears all memory objects from this buffer.
+	 */
+	public synchronized void clear() {
+		// memoryObjects.clear();
+
+		if (rawMemory != null)
+			for (int i = 0; i < memoryObjects.size(); i++) {
+				rawMemory.destroyMemoryObject(memoryObjects.get(i));
+			}
 		memoryObjects.clear();
 	}
+
+	/**
+	 * Prints the status of the Buffer.
+	 */
 	public synchronized void printStatus() {
 		System.out.println("###### Memory Buffer ########");
-		System.out.println("# Content: "+this.get());
-		System.out.println("# Size: "+this.get().size());
+		System.out.println("# Content: " + this.get());
+		System.out.println("# Size: " + this.get().size());
 		System.out.println("###############################");
 
 	}
