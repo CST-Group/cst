6cac5d10cb7abea2c1b4bd4ed72d9999e531d5f1
ricardo <ricardogp1993@gmail.com>
2018-12-17 17:48:23 -0200

    Addition of headers.

--- src/br/unicamp/cst/behavior/asbac/AffordanceExtractorCodelet.java
+++ src/br/unicamp/cst/behavior/asbac/AffordanceExtractorCodelet.java
@@ -1,3 +1,15 @@
+/**
+ * ****************************************************************************
+ * Copyright (c) 2018  DCA-FEEC-UNICAMP
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Contributors:
+ * R. G. Polizeli and R. R. Gudwin
+ * ****************************************************************************
+ */
 package br.unicamp.cst.behavior.asbac;
 
 import br.unicamp.cst.core.entities.Codelet;
--- src/br/unicamp/cst/behavior/asbac/Property.java
+++ src/br/unicamp/cst/behavior/asbac/Property.java
@@ -1,7 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ****************************************************************************
+ * Copyright (c) 2018  DCA-FEEC-UNICAMP
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Contributors:
+ * R. G. Polizeli and R. R. Gudwin
+ * ****************************************************************************
  */
 package br.unicamp.cst.behavior.asbac;
 
