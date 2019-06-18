d07d9b6709c94f66aa98e6a75b5ed882aa4b3e80
Andre Paraense <andre.paraense@mobwise.com.br>
2019-06-18 10:46:33 +0100

    bug loading resources

--- src/br/unicamp/cst/util/RendererJTree.java
+++ src/br/unicamp/cst/util/RendererJTree.java
@@ -26,7 +26,7 @@ public class RendererJTree extends DefaultTreeCellRenderer {
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean exp, boolean leaf, int row, boolean hasFocus) {
 
-        ImageIcon img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/configuration.png"));
+        ImageIcon img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/configuration.png"));
 
         DefaultMutableTreeNode dmtn = (DefaultMutableTreeNode) value;
         DefaultMutableTreeNode objectNode;
@@ -34,52 +34,52 @@ public class RendererJTree extends DefaultTreeCellRenderer {
         TreeElement node = (TreeElement) dmtn.getUserObject();
         switch (node.getIcon()) {
             case TreeElement.ICON_COMPOSITE:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/object.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/object.png"));
                 break;
             case TreeElement.ICON_AGGREGATE:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/object2.gif"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/object2.gif"));
                 break;    
             case TreeElement.ICON_PROPERTY:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/property.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/property.png"));
                 break;
             case TreeElement.ICON_QUALITYDIM:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/qualityDim.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/qualityDim.png"));
                 break;
             case TreeElement.ICON_VALUE:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/value.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/value.png"));
                 break;
             case TreeElement.ICON_MIND:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/mind.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/mind.png"));
                 break;
             case TreeElement.ICON_CODELET:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/codelet.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/codelet.png"));
                 break;
             case TreeElement.ICON_CODELETS:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/codelets.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/codelets.png"));
                 break;
             case TreeElement.ICON_MEMORY:
                 img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/memory.png"));
                 break;
             case TreeElement.ICON_MEMORIES:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/memories.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/memories.png"));
                 break;
             case TreeElement.ICON_CONTAINER:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/container.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/container.png"));
                 break;
             case TreeElement.ICON_MO:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/mo.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/mo.png"));
                 break;
             case TreeElement.ICON_INPUT:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/input.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/input.png"));
                 break;
             case TreeElement.ICON_OUTPUT:
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/output.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/output.png"));
                 break;
             case TreeElement.ICON_BROADCAST:    
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/broadcast.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/broadcast.png"));
                 break;
             case TreeElement.ICON_AFFORDANCE:    
-                img = new ImageIcon(this.getClass().getClassLoader().getResource("/br/unicamp/cst/images/codelet2.png"));
+                img = new ImageIcon(this.getClass().getClassLoader().getResource("br/unicamp/cst/images/codelet2.png"));
                 break;
             // TODO: Criar um icone de erro
         }
