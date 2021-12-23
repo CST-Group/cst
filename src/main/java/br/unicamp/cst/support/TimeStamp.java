cbd096f9081a1a06c130f2631ee79d2439a7778a
Ricardo Gudwin <rgudwin@gmail.com>
2021-12-23 17:59:27 -0300

    Updating banners

--- src/main/java/br/unicamp/cst/support/TimeStamp.java
+++ src/main/java/br/unicamp/cst/support/TimeStamp.java
@@ -1,8 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/***********************************************************************************************
+ * Copyright (c) 2012  DCA-FEEC-UNICAMP
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Contributors:
+ * K. Raizer, A. L. O. Paraense, E. M. Froes, R. R. Gudwin - initial API and implementation
+ ***********************************************************************************************/
 package br.unicamp.cst.support;
 
 import java.sql.Time;
